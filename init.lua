dce33b0eac381b598d097dff76be1350f7e0ffad
Michiel Lowijs <mlowijs@gmail.com>
2015-04-24 21:00:03 +0200

    Fixed indentation

--- init.lua
+++ init.lua
@@ -8,13 +8,13 @@ ready = false
 gpio.mode(4, gpio.INT, gpio.PULLUP)
 gpio.trig(4, "down", function(level) -- Falling edge means LED turns on
     tmr.stop(0)
-
-	tmr.alarm(0, 1125000, 0, function() -- Assuming the LED has a frequency of 1Hz
-		gpio.mode(4, gpio.OUTPUT)
-		gpio.write(4, gpio.LOW)
-		
-		ready = true
-	end)
+    
+    tmr.alarm(0, 1125000, 0, function() -- Assuming the LED has a frequency of 1Hz
+    	gpio.mode(4, gpio.OUTPUT)
+    	gpio.write(4, gpio.LOW)
+    	
+    	ready = true
+    end)
 end)
 
 srv = net.createServer(net.TCP)
