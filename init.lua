6f4abd353d0bf88ef4177137233cc8487f2c74f1
Michiel Lowijs <mlowijs@gmail.com>
2015-05-20 09:38:14 +0200

    Fixed pins numbers, removed some code, adding some dbg statements

--- init.lua
+++ init.lua
@@ -1,5 +1,4 @@
-tmr.delay(500000)
-available = true -- If the coffee pot does a "warm" boot, it's immediately available
+available = true
 
 -- Setup GPIO
 gpio.mode(5, gpio.INT, gpio.PULLUP)
@@ -8,22 +7,33 @@ gpio.trig(5, "up", function(level) -- Rising edge means LED turns off
     tmr.stop(0)
     
     tmr.alarm(0, 1250, 0, function()
+        print("I - Pot available")
         available = true
     end)
 end)
 
-gpio.mode(1, gpio.OUTPUT) -- Espresso
+gpio.mode(1, gpio.OUTPUT) -- Lungo (GPIO 5)
 gpio.write(1, gpio.HIGH)
 
-gpio.mode(2, gpio.OUTPUT) -- Lungo
+gpio.mode(2, gpio.OUTPUT) -- Espresso (GPIO 4)
 gpio.write(2, gpio.HIGH)
 
--- Setup TCP server
 srv = net.createServer(net.TCP)
 srv:listen(80, function(conn)
     conn:on("receive", function(conn, data)
-        local headers = getHeaders(data)
-        local _, _, verb = data:find("(%w+) ")
+        isVerb = true
+		verb = nil
+        headers = {}
+    
+        for line in data:gmatch("%C+") do    
+            if isVerb then
+				_, _, verb = line:find("(%w+) ")
+                isVerb = false
+            else
+                _, _, key, value = line:find("(.+): (.+)")
+                headers[key] = value
+            end
+        end
     
         if verb == "GET" then
             handleGet(conn, headers)
@@ -33,8 +43,10 @@ srv:listen(80, function(conn)
     end)
 end)
 
--- Request handlers
+print("I - Pot booted")
+
 function handleGet(conn, headers)
+    print("I - Serving web page")
     local responseHeaders = {["Content-Type"] = "text/html"}
 
     file.open("index.html", "r")
@@ -44,51 +56,37 @@ end
 
 function handleBrew(conn, headers)
     if not available then
+        print("E - Cannot brew, pot unavailable")
         endResponse(conn, 503, "Service Unavailable", nil, "The coffee pot is not available right now.")
         return
     end
     
     if headers["Accept-Additions"] then
+        print("E - Additions not accepted")
         endResponse(conn, 406, "Not Acceptable", nil, "This coffee pot does not accept additions.")
         return
     end
     
     pin = 0
     if headers["X-Coffee-Variation"] == "espresso" then
-        pin = 1
-    elseif headers["X-Coffee-Variation"] == "lungo" then
         pin = 2
+    elseif headers["X-Coffee-Variation"] == "lungo" then
+        pin = 1
     else
+        print("E - Coffee variation invalid or not specified")
         endResponse(conn, 400, "Bad Request", nil, "Please provide a valid X-Coffee-Variation header (espresso | lungo).")
         return
     end
 
-    print("Brewing some " .. headers["X-Coffee-Variation"])
-    
-    gpio.write(pin, gpio.LOW)
-    tmr.delay(200000)
-    gpio.write(pin, gpio.HIGH)
+    print("I - Brewing some " .. headers["X-Coffee-Variation"])
 
     local responseHeaders = {["Content-Type"] = "message/coffeepot",
                              ["Safe"] = "yes"}
     endResponse(conn, 200, "OK", responseHeaders, "start")
-end
 
--- HTTP utilities
-function getHeaders(data)
-    isFirst = true
-    headers = {}
-
-    for line in data:gmatch("%C+") do    
-        if isFirst then
-            isFirst = false
-        else
-            _, _, key, value = line:find("(.+): (.+)")
-            headers[key] = value
-        end
-    end
-
-    return headers
+    gpio.write(pin, gpio.LOW)
+    tmr.delay(500000)
+    gpio.write(pin, gpio.HIGH)
 end
 
 function endResponse(conn, statusCode, statusMessage, headers, data)
